4ff5263523a556ee56da8e215c5d6155fcc80149
huhututu0599 <1534677036@qq.com>
2024-01-06 23:29:57 +0800

    first commit create file

---
+++ printlabel/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace printlabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start_server = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gen_barcode_print = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_start_server
+            // 
+            this.btn_start_server.Location = new System.Drawing.Point(145, 43);
+            this.btn_start_server.Name = "btn_start_server";
+            this.btn_start_server.Size = new System.Drawing.Size(75, 23);
+            this.btn_start_server.TabIndex = 0;
+            this.btn_start_server.Text = "button1";
+            this.btn_start_server.UseVisualStyleBackColor = true;
+            this.btn_start_server.Click += new System.EventHandler(this.btn_start_server_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 125);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gen_barcode_print
+            // 
+            this.gen_barcode_print.Location = new System.Drawing.Point(145, 85);
+            this.gen_barcode_print.Name = "gen_barcode_print";
+            this.gen_barcode_print.Size = new System.Drawing.Size(75, 23);
+            this.gen_barcode_print.TabIndex = 3;
+            this.gen_barcode_print.Text = "button1";
+            this.gen_barcode_print.UseVisualStyleBackColor = true;
+            this.gen_barcode_print.Click += new System.EventHandler(this.gen_barcode_print_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 432);
+            this.Controls.Add(this.gen_barcode_print);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_start_server);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start_server;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button gen_barcode_print;
+    }
+}
+
---
+++ printlabel/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using ZXing;
+using System.Drawing.Printing;
+namespace printlabel
+{
+    public partial class Form1 : Form
+    {
+        HttpListener listener;
+        private PrintDocument printDocument;
+        public Form1()
+        {
+            InitializeComponent();
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:4555/");
+            printDocument = new PrintDocument();
+            printDocument.PrintPage += PrintDocument_PrintPage;
+        }
+
+        private void btn_start_server_Click(object sender, EventArgs e)
+        {
+            listener.Start();
+
+            listener.BeginGetContext(new AsyncCallback(OnRequestReceive), null);
+
+        }
+        private void OnRequestReceive(IAsyncResult ar)
+        {
+            var context = listener.EndGetContext(ar);
+            listener.BeginGetContext(new AsyncCallback(OnRequestReceive), null);
+
+            if (context.Request.HttpMethod == "POST")
+            {
+                using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                {
+                    string content = reader.ReadToEnd();
+                    // 在这里处理POST数据
+                    Console.WriteLine(content);
+                }
+            }
+
+            byte[] buffer = Encoding.UTF8.GetBytes("Request received!");
+            context.Response.ContentLength64 = buffer.Length;
+            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            context.Response.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            listener.Stop();
+        }
+
+        private void gen_barcode_print_Click(object sender, EventArgs e)
+        {
+            // Generate barcode data (e.g., a sample text)
+            string barcodeData = "123456789";
+
+            // Create an instance of BarcodeWriter
+            BarcodeWriter barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.CODE_128; // You can choose other barcode formats
+
+            // Generate a barcode bitmap
+            Bitmap barcodeBitmap = barcodeWriter.Write(barcodeData);
+
+            // Display the barcode in PictureBox
+            pictureBox1.Image = barcodeBitmap;
+
+
+
+            // Show the PrintDialog to select the printer
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Set the printer to the selected printer
+                printDocument.PrinterSettings.PrinterName = printDialog.PrinterSettings.PrinterName;
+
+                // Print the document
+                //printDocument.Print();
+                printDocument.Print();
+            }
+
+
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            // Print the image in the PictureBox
+            e.Graphics.DrawImage(pictureBox1.Image, 0, 0);
+        }
+
+    }
+}
